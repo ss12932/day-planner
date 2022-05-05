51b5100ff0c762211e3d4a31f5e53e5d1faef2aa
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-05 13:24:52 +0100

    working on hour js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-'use strict mode'
+"use strict mode";
+$(document).ready(() => {
+  let currDate = moment().format("dddd Do MMMM YYYY, hh:mm:ss");
+  $("#currentDay").text(currDate);
+});
+
+const hrArr = [];
+new Array(10).fill().forEach((_, i) => {
+  hrArr.push(moment({ hour: `${i + 9}` }).format("H"));
+});
+
+$.each(hrArr, (_, hr) => {
+  const row = $("<div class='row bg-light'></div>");
+  const hourCol = $(
+    "<div class='col-lg-2 bg-primary d-flex justify-content-start align-items-center'>" +
+      hr +
+      (hr > 12 ? "PM" : "AM") +
+      "</div>"
+  );
+  const txtAreaCol = $("<div class='col-lg-8 bg-light'></div>");
+  const saveClrCol = $("<div class='col-lg-2 bg-danger'></div>");
+  row.append(hourCol, txtAreaCol, saveClrCol);
+  $(".container").append(row);
+});
