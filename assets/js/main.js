fe2fe3839a2eefc39ca8367c928f38679c30723e
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-05 16:05:31 +0100

    testing save btn js logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,20 @@ $(document).ready(() => {
   let currDate = moment().format("dddd Do MMMM YYYY, hh:mm:ss");
   $("#currentDay").text(currDate);
 });
-
 const hrArr = [];
 new Array(10).fill().forEach((_, i) => {
   hrArr.push(moment({ hour: `${i + 9}` }).format("h A"));
 });
 
+const saveBtnClick = (e) => {
+  if ($(e.target).is(".fa-floppy-disk")) {
+    console.log("you clicked on the save button");
+    alert("save button pressed");
+  }
+};
+
 $.each(hrArr, (_, hr) => {
-  const row = $("<div class='row bg-light d-flex gap'></div>");
+  const row = $("<div class='row bg-light d-flex'></div>");
   const hourCol = $(
     "<div class='col-1 bg-light d-flex justify-content-start align-items-center'>" +
       hr +
@@ -21,8 +27,10 @@ $.each(hrArr, (_, hr) => {
     "<div class='col-10 bg-light input-group'><textarea class='form-control' aria-label='with textarea'></textarea></div>"
   );
   const saveClrCol = $(
-    "<div class='col-1 bg-light d-flex align-items-center justify-content-around pl-0'><i class='fa-solid fa-floppy-disk fa-lg'></i><i class='fa-solid fa-trash-can fa-lg'></i></div>"
+    "<div class='col-1 bg-light d-flex align-items-center justify-content-around pl-0'><i data-hour=`{hr}` class='fa-solid fa-floppy-disk fa-lg'></i><i data-hour=`{hr}` id='reset-btn' class='fa-solid fa-trash-can fa-lg'></i></div>"
   );
+  // retrieveLS(i);
   row.append(hourCol, txtAreaCol, saveClrCol);
   $(".container").append(row);
+  row.on("click", saveBtnClick);
 });
